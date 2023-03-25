a328e96a73178650b9931607fd80a9a91f5c6746
mitsukaki <lite260@gmail.com>
2023-03-25 16:10:33 -0600

    receipt scanning

--- src/endpoints/receipt.js
+++ src/endpoints/receipt.js
@@ -1,26 +1,59 @@
 const tesseract = require("node-tesseract-ocr")
+const { v4: uuidv4 } = require('uuid');
 const config = {
     lang: "eng",
     oem: 1,
     psm: 3,
 }
 
+const item_regex = /^([a-zA-Z\s\d]+)\s([\d]{5,})/gm
+
 exports.handler = (req, res, next) => {
-    // req.file is the `scan` file
-    // req.body will hold the text fields, if there were any
-    console.log(req.file)
+    // get two weeks from today date in ISO format
+    let twoWeeksFromNow = new Date()
+    twoWeeksFromNow.setDate(twoWeeksFromNow.getDate() + 14)
+    twoWeeksFromNow = twoWeeksFromNow.toISOString()
 
     tesseract.recognize("uploads/" + req.file.filename, config).then((text) => {
-        console.log(text)
+        let items = {}
 
-        // status 200
-        res.status(200).json(text)
+        // use regex to get all the items
+        let matches = text.matchAll(item_regex)
+
+        // add all matches to the items
+        for (const match of matches) {
+            // if we've seen this item before, increment the quantity
+            if (items.hasOwnProperty(match[1])) {
+                items[match[1]].quantity += 1
+            } else {
+                // create the item
+                let item = {
+                    name: capitalizeWords(match[1].trim()),
+                    _id: uuidv4(),
+                    expiry: twoWeeksFromNow,
+                    quantity: 1
+                }
+
+                items[item._id] = item
+            }
+        }
+
+        // return the items
+        res.status(200).json(items)
     })
     .catch((err) => {
-        //  send the error
+        // send the error
         console.log(err)
         res.status(err.statusCode).json({
             message: err.message
         })
     })
+}
+
+function capitalizeWords(str) {
+    return str
+        .toLowerCase()
+        .split(' ')
+        .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+        .join(' ');
 }
